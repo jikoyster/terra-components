e3fcd710a742b4aff379a5c519dfd868bea74372
jikoyster <yu.jake@gmail.com>
2026-05-06 08:32:10 +0000

    feat(farms): replace text loader with centered spinner
    Replace the plain "Loading..." text with a centered Oval spinner from
    react-loader-spinner to improve loading UX. Add react-loader-spinner
    dependency in package.json and import the Oval component in
    Farms.tsx. The spinner is rendered while useFarms reports loading.

--- src/components/farms/Farms.tsx
+++ src/components/farms/Farms.tsx
@@ -1,10 +1,22 @@
 import { useFarms } from "@/hooks/useFarms";
-import FarmList from "./FarmList"
+import FarmList from "./FarmList";
+import { Oval } from "react-loader-spinner";
 
 export default function Farm() {
   const { loading } = useFarms();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className="flex min-h-[100svh] w-full items-center justify-center">
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        visible={true}
+        ariaLabel="loading"
+      />
+    </div>
+  );
+
 
   return (
     <FarmList />
